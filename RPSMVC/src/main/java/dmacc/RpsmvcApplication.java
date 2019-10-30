0fa87fd1f751b74bbb5e9ff149f8202049aa439d
lauraamejia <lauramejiat16@gmail.com>
2019-10-30 18:47:31 -0500

    final commit

---
+++ RPSMVC/src/main/java/dmacc/RpsmvcApplication.java
@@ -0,0 +1,13 @@
+package dmacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpsmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpsmvcApplication.class, args);
+	}
+
+}
